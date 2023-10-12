5687742ecba7f0dd84d4e472572351f33b50f455
Francisco Montesino <mfranciscoignacio@gmail.com>
2023-10-12 09:42:36 -0300

    menu y carrito

--- scripts.js
+++ scripts.js
@@ -1,5 +1,39 @@
-function toggleDropdown(){
-    let menu = document.getElementById("myDropdown");
-    menu.classList.toggle("show");
-}
+const iconoMenu = document.querySelector('#icono-menu'),
+      menu = document.querySelector('#menu');
+
+iconoMenu.addEventListener('click', (e) => {
+
+    
+    menu.classList.toggle('active');
+    document.body.classList.toggle('opacity');
+
+    
+    const rutaActual = e.target.getAttribute('src');
+
+    if(rutaActual == "assets/menu.svg"){
+        e.target.setAttribute('src',"assets/menu2.svg");
+    }else{
+        e.target.setAttribute('src',"assets/menu.svg");
+    }
+});
+
+
+const iconoCarrito = document.querySelector('#icono-carrito'),
+      carrito = document.querySelector('#carrito');
+
+iconoCarrito.addEventListener('click', (e) => {
+
+    
+    carrito.classList.toggle('active');
+    document.body.classList.toggle('opacity');
+
+    
+    const rutaActual = e.target.getAttribute('src');
+
+    if(rutaActual == "assets/carrito.svg"){
+        e.target.setAttribute('src',"assets/carrito.svg");
+    }else{
+        e.target.setAttribute('src',"assets/carrito.svg");
+    }
+});
 
